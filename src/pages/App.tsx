fb614a377b923e90a77ea84940eccea6f1f5798e
Tiago Andrade <tiago22900@gmail.com>
2022-06-29 15:58:13 -0400

    att

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import Container from '@mui/material/Container';
+import Router from '../routes';
+
+
+
+export default function App() {
+
+  return (
+    <Container maxWidth="sm">
+    <Router />
+    </Container>
+
+  );
+}
+
+
